3827551b2a327f1dbb2ded8ca66188d8c17382dd
rohann799 <rohann799gmail.com>
2021-07-29 16:21:56 +0530

    complete second project

---
+++ MavenProject/Day40-Jenkinswithmaven/src/main/java/com/star/demo/Mainapp.java
@@ -0,0 +1,13 @@
+package com.star.demo;
+
+public class Mainapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Working in jenkins");
+		System.out.println("hello");
+		
+	}
+
+}
