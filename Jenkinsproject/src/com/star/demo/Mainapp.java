f5bdd64f85d863c81d7ba2903701a72456f843b2
rohann799 <rohann799gmail.com>
2021-07-29 13:04:29 +0530

    DAY40-Jenkinsconnection

---
+++ Jenkinsproject/src/com/star/demo/Mainapp.java
@@ -0,0 +1,12 @@
+package com.star.demo;
+
+public class Mainapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Working in jenkins");
+		
+	}
+
+}
